20a95edc239df8f101a77f21a3a5c6ab3159612c
Jeremie Rodriguez <contact@jeremierodriguez.com>
2022-02-26 23:49:34 -0500

    feat: accept both AMD64 & ARM64 architectures

--- Formula/nas-cli.rb
+++ Formula/nas-cli.rb
@@ -2,14 +2,26 @@ class NasCli < Formula
   version "22.01.4"
   desc "CLI application for managing my NAS."
   homepage "https://github.com/jeremiergz/nas-cli"
-  url "https://github.com/jeremiergz/nas-cli/releases/download/#{version}/nas-cli-darwin-amd64"
   head "https://github.com/jeremiergz/nas-cli.git"
   license "Apache-2.0"
   conflicts_with "nas-cli"
-  sha256 "35ee0d995278078e3b7c7fbd7cab3db65721ca0dd6017b8323469d0ccb0bb50c"
+
+  if Hardware::CPU.intel?
+    url "https://github.com/jeremiergz/nas-cli/releases/download/#{version}/nas-cli-darwin-amd64"
+    sha256 ""
+  end
+  if Hardware::CPU.arm?
+    url "https://github.com/jeremiergz/nas-cli/releases/download/#{version}/nas-cli-darwin-arm64"
+    sha256 ""
+  end
 
   def install
-    bin.install "nas-cli-darwin-amd64" => "nas-cli"
+    if Hardware::CPU.intel?
+      bin.install "nas-cli-darwin-amd64" => "nas-cli"
+    end
+    if Hardware::CPU.arm?
+      bin.install "nas-cli-darwin-arm64" => "nas-cli"
+    end
   end
 
   test do
